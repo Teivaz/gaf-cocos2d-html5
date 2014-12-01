d05650a800beee87976a5154201f24df8de7e15a
Teivaz <o.kuznietsov@gmail.com>
2014-12-02 00:26:43 +0200

    + Added gaf scrips loader function + Added gaf resources load handler

--- GAF.js
+++ GAF.js
@@ -1,5 +1,6 @@
 var gaf = gaf || {};
 
+
 gaf.IDNONE = cc.UINT_MAX;
 gaf.FIRST_FRAME_INDEX = 0;
 
@@ -11,3 +12,30 @@ gaf.ACTION_DISPATCH_EVENT = 4;
 
 gaf.PI_FRAME = 0;
 gaf.PI_EVENT_TYPE = 0;
+
+
+gaf.LoadSources = function() {
+
+    var gaf_dir = "frameworks/gaf-cocos2d-html5/";
+
+    var sources = [
+    "Library/GAFTags.js",
+    "Library/GAFObject.js",
+    "Library/GAFLoader.js",
+    "Library/GAFDataReader.js",
+    "Library/GAFAsset.js"];
+
+    sources.forEach(function(source){
+        var s = cc.newElement('script');
+        s.src = gaf_dir + source;
+        document.body.appendChild(s);
+    });
+};
+
+gaf.CCGAFLoader = function(){
+    this.load = function(realUrl, url, item, cb){
+        var loader = new gaf.Loader();
+        loader.LoadFile(realUrl, function(data){cb(null, data)});
+    };
+};
+cc.loader.register('.gaf', new gaf.CCGAFLoader());
