48414d105ee7a8b8f8aabe0395f8e26bbb57d7e5
teivaz <teivaz@rambler.ru>
2014-12-01 16:49:22 +0200

    - Interface for GAFAsset

--- Library/GAFAsset.js
+++ Library/GAFAsset.js
@@ -9,7 +9,7 @@ gaf.GAFAsset = cc.Class.extend({
      * @return {bool}
      */
     initWithGAFFile : function(filePath, delegate){
-
+        debugger;
     },
 
     /**
@@ -20,32 +20,7 @@ gaf.GAFAsset = cc.Class.extend({
      * @return {bool}
      */
     initWithGAFBundle : function(zipFilePath, entryFile, delegate){
-
-    },
-
-    /**
-     * @method pushTimeline
-     * @param {unsigned int} arg0
-     * @param {gaf::GAFTimeline} arg1
-     */
-    pushTimeline : function(timelineIdRef, GAFTimeline){
-
-    },
-
-    /**
-     * @method setHeader
-     * @param {gaf::GAFHeader} arg0
-     */
-    setHeader : function(GAFHeader){
-
-    },
-
-    /**
-     * @method setRootTimeline
-     * @param {gaf::GAFTimeline} arg0
-     */
-    setRootTimeline : function(timeline){
-
+        debugger;
     },
 
     /**
@@ -53,7 +28,7 @@ gaf.GAFAsset = cc.Class.extend({
      * @param {String} arg0
      */
     setRootTimelineWithName : function(name){
-
+        debugger;
     },
 
     /**
@@ -61,66 +36,170 @@ gaf.GAFAsset = cc.Class.extend({
      * @return {gaf::GAFTimeline}
      */
     getRootTimeline : function(){
-
+        debugger;
     },
 
     /**
      * @method getTimelines
-     * @return {map_object|map_object}
+     * @return {[gaf::GAFTimeline]}
      */
     getTimelines : function(){
-
+        debugger;
     },
-    createWithBundle : function(){
 
+    /**
+     * @method createWithBundle
+     * @param {String} zipFilePath - path to the archive with .gaf and its textures
+     * @param {String} entryFile - name of the .gaf file in archive
+     * @param {function({path:String})} delegate - is used to change atlas path, e.g. to load `atlas.tga` instead of `atlas.png`
+     * @return {gaf::GAFAsset}
+     */
+    createWithBundle : function(zipFilePath, entryFile, delegate){
+        var asset = new gaf.GAFAsset();
+        asset.initWithGAFBundle(zipFilePath, entryFile, delegate);
+        return asset;
     },
-    create : function(){
 
-    },
     isAssetVersionPlayable : function(){
-
+        return true;
     },
+
+    /**
+     * @method initWithGAFFile
+     * @param {String} filePath - path to .gaf file
+     * @param {function({path:String})} delegate - is used to change atlas path, e.g. to load `atlas.tga` instead of `atlas.png`
+     * @return {gaf::GAFAsset}
+     */
+    create : function (gafFilePath, delegate ){
+        var asset = new gaf.GAFAsset();
+        asset.initWithGAFFile(gafFilePath, delegate);
+        return asset;
+    },
+
+    /**
+     * @method createObject
+     * @return {gaf::GAFObject}
+     */
     createObject : function(){
-
+        debugger;
     },
+
+    /**
+     * @method createObjectAndRun
+     * [@param {boolean} arg0 - run looped. False by default]
+     * @return {gaf::GAFObject}
+     */
     createObjectAndRun : function(){
+        var looped = false;
+        if(arguments.length == 1) {
+            looped = arguments[0];
+        }
 
+        debugger;
     },
+
+    /**
+     * @method desiredCsf
+     * @return {float}
+     */
     desiredCsf : function(){
-
+        debugger;
     },
-    setDesiredCsf : function(){
 
+    /**
+     * @method setDesiredCsf
+     * @param {float} arg0
+     */
+    setDesiredCsf : function(csf){
+        debugger;
     },
-    setTextureLoadDelegate : function(){
 
+    /**
+     * @method setTextureLoadDelegate
+     * @param {function} arg0
+     */
+    setTextureLoadDelegate : function(delegate){
+        debugger;
     },
+
+    /**
+     * @method getTextureManager
+     * @return {gaf::GAFAssetTextureManager}
+     */
     getTextureManager : function(){
-
+        debugger;
     },
+
+    /**
+     * @method getSceneFps
+     * @return {unsigned int}
+     */
     getSceneFps : function(){
-
+        debugger;
     },
+
+    /**
+     * @method getSceneWidth
+     * @return {unsigned int}
+     */
     getSceneWidth : function(){
-
+        debugger;
     },
+
+    /**
+     * @method getSceneHeight
+     * @return {unsigned int}
+     */
     getSceneHeight : function(){
-
+        debugger;
     },
+
+    /**
+     * @method getSceneColor
+     * @return {color4b_object}
+     */
     getSceneColor : function(){
-
+        debugger;
     },
+
+    /**
+     * @method setSceneFps
+     * @param {unsigned int} arg0
+     */
     setSceneFps : function(fps){
-
+        debugger;
     },
+
+    /**
+     * @method setSceneWidth
+     * @param {unsigned int} arg0
+     */
     setSceneWidth : function(width){
-
+        debugger;
     },
+
+    /**
+     * @method setSceneHeight
+     * @param {unsigned int} arg0
+     */
     setSceneHeight : function(height){
-
+        debugger;
     },
+
+    /**
+     * @method setSceneColor
+     * @param {color4b_object} arg0
+     */
     setSceneColor : function(color4B){
+        debugger;
+    },
 
+    /**
+     * @method getHeader
+     * @return {gaf::GAFHeader}
+     */
+    getHeader : function ()
+    {
+        debugger;
     }
-
 });
