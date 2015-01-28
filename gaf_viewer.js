1eaf8449859c9716f1fab0064976d5f71ad7c7e0
teivaz <teivaz@rambler.ru>
2015-01-28 16:59:35 +0200

    - New system for preview gaf

--- gaf_viewer.js
+++ gaf_viewer.js
@@ -1,8 +1,13 @@
- /*
- * Created by Teivaz on 29.11.2014.
- * Thanks to David Caldwell <david@porkrind.org> for `renderjson`
- */
-function handleFileSelect(evt) {
+/*
+* Created by Teivaz on 29.11.2014.
+* Thanks to David Caldwell <david@porkrind.org> for `renderjson`
+*/
+
+var g_hasData = false;
+var g_idDropZone = null;
+var g_idFade = null;
+
+function handleDrop(evt) {
     evt.stopPropagation();
     evt.preventDefault();
 
@@ -19,12 +24,50 @@ function handleFileSelect(evt) {
                     var arrayBuffer = new gaf.DataReader(req.target.result);
                     var loader = new gaf.Loader();
                     var data = loader.LoadStream(arrayBuffer);
-                    document.getElementById('list').appendChild(renderjson(data));
+                    g_hasData = true;
+                    g_idFade.style.display = "none";
+                    g_idDropZone.style.display = "none";
+                    document.getElementById('output').appendChild(stringifyGaf("GAF", data, false));
                 };
             })(f);
             reader.readAsArrayBuffer(f);
         }
     }
+}
+
+function initGlobals() {
+    g_idDropZone = document.getElementById('drop_zone');
+    g_idFade = document.getElementById('fade');
+}
+
+function bodyOnLoad(){
+    initGlobals();
+
+    g_idDropZone.addEventListener('dragover', handleDragOver, false);
+    g_idDropZone.addEventListener('dragenter', handelDragEnter, false);
+    g_idDropZone.addEventListener('dragleave', handelDragLeave, false);
+    g_idDropZone.addEventListener('drop', handleDrop, false);
+    document.body.addEventListener('dragover', bodyDrag, false);
+}
+
+function handelDragEnter(evt){
+    evt.stopPropagation();
+    evt.preventDefault();
+    console.log("handelDragEnter");
+    g_idDropZone.style.display = "block";
+    if(g_hasData) {
+        g_idFade.style.display = "block";
+    }
+}
+
+function handelDragLeave(evt){
+    evt.stopPropagation();
+    evt.preventDefault();
+     console.log("handelDragLeave");
+     g_idFade.style.display = "none";
+     if(g_hasData) {
+         g_idDropZone.style.display = "none";
+     }
 }
 
 function handleDragOver(evt) {
@@ -33,187 +76,104 @@ function handleDragOver(evt) {
     evt.dataTransfer.dropEffect = 'copy';
 }
 
-var dropZone = document.getElementById('drop_zone');
-dropZone.addEventListener('dragover', handleDragOver, false);
-dropZone.addEventListener('drop', handleFileSelect, false);
+function bodyDrag(){
+    g_idDropZone.style.display = "block";
+    console.log("bodyDrag");
+}
 
-var module;
-(module || {}).exports = renderjson = (function() {
-    var themetext = function( /* [class, text]+ */ ) {
-        var spans = [];
-        while (arguments.length)
-            spans.push(append(span(Array.prototype.shift.call(arguments)),
-                text(Array.prototype.shift.call(arguments))));
-        return spans;
-    };
-    var append = function( /* el, ... */ ) {
-        var el = Array.prototype.shift.call(arguments);
-        for (var a = 0; a < arguments.length; a++)
-            if (arguments[a].constructor == Array)
-                append.apply(this, [el].concat(arguments[a]));
-            else
-                el.appendChild(arguments[a]);
-        return el;
-    };
-    var prepend = function(el, child) {
-        el.insertBefore(child, el.firstChild);
-        return el;
-    };
-    var isempty = function(obj) {
-        for (var k in obj)
-            if (obj.hasOwnProperty(k)) return false;
-        return true;
-    };
-    var text = function(txt) {
-        return document.createTextNode(txt)
-    };
-    var div = function() {
-        return document.createElement("div")
-    };
-    var span = function(classname) {
-            var s = document.createElement("span");
-        if (classname) s.className = classname;
-        return s;
-    };
-    var A = function A(txt, classname, callback) {
-        var a = document.createElement("a");
-        if (classname) a.className = classname;
-        a.appendChild(text(txt));
-        a.href = '#';
-        a.onclick = function() {
-            callback();
-            return false;
-        };
-        return a;
-    };
+function stringifyGaf(name, gaf, recursive){
+    var gafItem = createItem(name, gaf);
+    if(isExtendable(gaf, name)){
+        //if(recursive)
+        //    buildGafNode(gafItem, recursive);
+        //else
+            buildPreview(gafItem);
+    }
+    else{
+        buildGafNode(gafItem, false);
+    }
+    return gafItem;
+}
 
-    function _renderjson(json, indent, dont_indent, show_level, sort_objects) {
-        var my_indent = dont_indent ? "" : indent;
+function createItem(name, object){
+    var item = document.createElement('div');
+    item.gafObject = object;
+    item.className = 'item';
 
-        if (json === null) return themetext(null, my_indent, "keyword", "null");
-        if (json === void 0) return themetext(null, my_indent, "keyword", "undefined");
-        if (typeof(json) != "object") // Strings, numbers and bools
-            return themetext(null, my_indent, typeof(json), JSON.stringify(json));
+    item.gafName = document.createElement('span');
+    item.gafName.className = 'itemName';
+    item.gafName.innerText = name;
+    item.appendChild(item.gafName);
 
-        var disclosure = function(open, close, type, builder) {
-            var content;
-            var empty = span(type);
-            var show = function() {
-                if (!content) append(empty.parentNode,
-                    content = prepend(builder(),
-                        A(renderjson.hide, "disclosure",
-                            function() {
-                                content.style.display = "none";
-                                empty.style.display = "inline";
-                            })));
-                content.style.display = "inline";
-                empty.style.display = "none";
-            };
+    var br = document.createElement('br');
+    item.appendChild(br);
 
-            function isColor(a){
-                return a.hasOwnProperty('a') && a.hasOwnProperty('r') && a.hasOwnProperty('g') && a.hasOwnProperty('b');
-            }
+    item.gafContainer = document.createElement('div');
+    item.gafContainer.className = 'expandable';
+    item.appendChild(item.gafContainer);
 
-            var color_rect = span();
-            if (json.hasOwnProperty("tagName"))
-                var placeholder = json.tagName;
-            else if (json.hasOwnProperty("header"))
-                placeholder = " GAF v" + json.header.versionMajor + "." + json.header.versionMinor + " ";
-            else if (json.constructor == Array)
-                placeholder = " " + json.length + " ";
-            else if (json.hasOwnProperty("id"))
-                placeholder = " id:" + json.id + " ... ";
-            else if (json.hasOwnProperty("objectId"))
-                placeholder = " id:" + json.objectId + " ... ";
-            else if (json.hasOwnProperty("frame"))
-                placeholder = " frame:" + json.frame + " ... ";
-            else if(isColor(json)){
-                color_rect.style.backgroundColor = "rgba("+json.r+","+json.g+","+json.b+","+json.a / 255.0+")";// parseInt(json.r).toString(16) + parseInt(json.g).toString(16) + parseInt(json.b).toString(16);
-                color_rect.style.height = '10px';
-                color_rect.style.width = '10px';
-                color_rect.style.display = 'inline-block';
-                color_rect.style.margin = '0 4px';
-                color_rect.style.border = '1px solid #7f7f7f';
-            }
+    item.gafBuilt = false;
 
-            placeholder = placeholder || ' ... ';
-            append(empty,
-                A(renderjson.show, "disclosure", show),
-                color_rect,
-                themetext(type + " syntax", open),
-                A(placeholder, null, show),
-                themetext(type + " syntax", close));
-
-            var el = append(span(), text(my_indent.slice(0, -1)), empty);
-            if (show_level > 0)
-                show();
-            return el;
-        };
-
-        if (json.constructor == Array) {
-            if (json.length == 0) return themetext(null, my_indent, "array syntax", "[]");
-
-            return disclosure("[", "]", "array", function() {
-                var as = append(span("array"), themetext("array syntax", "[", null, "\n"));
-                for (var i = 0; i < json.length; i++)
-                    append(as,
-                        _renderjson(json[i], indent + "    ", false, show_level - 1, sort_objects),
-                        i != json.length - 1 ? themetext("syntax", ",") : [],
-                        text("\n"));
-                append(as, themetext(null, indent, "array syntax", "]"));
-                return as;
-            });
+    item.onclick = function(ev) {
+        ev.stopPropagation();
+        if(item.gafBuilt) {
+            item.gafContainer.style.display = (item.gafContainer.style.display === 'none') ? 'block' : 'none';
         }
+        else{
+            buildGafNode(item, false);
+        }
+    };
 
-        // object
-        if (isempty(json))
-            return themetext(null, my_indent, "object syntax", "{}");
+    return item;
+}
 
+function buildPreview(obj) {
+    var container = obj.gafContainer;
+    //container.appendChild(createItem(JSON.stringify(obj)));
+    container.appendChild(createItem("..."));
+}
 
-        return disclosure("{", "}", "object", function() {
-            var os = append(span("object"), themetext("object syntax", "{", null, "\n"));
-            for (var k in json) var last = k;
-            var keys = Object.keys(json);
-            if (sort_objects)
-                keys = keys.sort();
-            for (var i in keys) {
-                var k = keys[i];
-                append(os, themetext(null, indent + "    ", "key", '"' + k + '"', "object syntax", ': '),
-                    _renderjson(json[k], indent + "    ", true, show_level - 1, sort_objects),
-                    k != last ? themetext("syntax", ",") : [],
-                    text("\n"));
+function buildGafNode(obj, recursive){
+    var item;
+    var gaf = obj.gafObject;
+    var container = obj.gafContainer;
+    obj.gafBuilt = true;
+
+    if (gaf instanceof String) {
+        container.appendChild(createItem(gaf));
+    }
+    else if (typeof gaf === 'number') {
+        container.appendChild(createItem(gaf));
+    }
+    else if (gaf instanceof Array) {
+        for (var i = 0, length = gaf.length; i < length; ++i) {
+            item = gaf[i];
+            if (typeof item !== 'undefined') {
+                container.appendChild(stringifyGaf(i, item, recursive));
             }
-            append(os, themetext(null, indent, "object syntax", "}"));
-            return os;
-        });
+        }
+    }
+    else /*if(gaf instanceof Object)*/{
+        try {
+            for (item in gaf) {
+                if (gaf.hasOwnProperty(item)) {
+                    container.appendChild(stringifyGaf(item, gaf[item], recursive));
+                }
+            }
+        }
+        catch (e) {
+            container.appendChild(createItem("..."));
+        }
+    }
+}
+
+function isExtendable(obj, name){
+    if (typeof obj === 'number'){
+        return false;
+    }
+    if(obj instanceof String){
+        return false;
     }
 
-    var renderjson = function renderjson(json) {
-        var pre = append(document.createElement("pre"), _renderjson(json, "", false, renderjson.show_to_level, renderjson.sort_objects));
-        pre.className = "renderjson";
-        return pre;
-    };
-    renderjson.set_icons = function(show, hide) {
-        renderjson.show = show;
-        renderjson.hide = hide;
-        return renderjson;
-    };
-    renderjson.set_show_to_level = function(level) {
-        renderjson.show_to_level = typeof level == "string" &&
-            level.toLowerCase() === "all" ? Number.MAX_VALUE : level;
-        return renderjson;
-    };
-    renderjson.set_sort_objects = function(sort_bool) {
-        renderjson.sort_objects = sort_bool;
-        return renderjson;
-    };
-    // Backwards compatiblity. Use set_show_to_level() for new code.
-    renderjson.set_show_by_default = function(show) {
-        renderjson.show_to_level = show ? Number.MAX_VALUE : 0;
-        return renderjson;
-    };
-    renderjson.set_icons('⊕', '⊖');
-    renderjson.set_show_by_default(false);
-    renderjson.set_sort_objects(false);
-    return renderjson;
-})();
+    return true;
+}
