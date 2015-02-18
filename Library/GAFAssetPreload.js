feb260ff8dc0931a3f2d8915b968174fdceb4746
teivaz <teivaz@rambler.ru>
2015-02-18 11:34:28 +0200

    - Refactored GAFAssetPreload.js

--- Library/GAFAssetPreload.js
+++ Library/GAFAssetPreload.js
@@ -13,37 +13,23 @@ gaf.CGAffineTransformCocosFormatFromFlashFormat = function(transform)
 
 gaf._AssetPreload = function()
 {
-    this["0"] = gaf._AssetPreload.End;
-    this["1"] = gaf._AssetPreload.Atlases;
-    this["2"] = gaf._AssetPreload.AnimationMasks;
-    this["3"] = gaf._AssetPreload.AnimationObjects;
-    this["4"] = gaf._AssetPreload.AnimationFrames;
-    this["5"] = gaf._AssetPreload.NamedParts;
-    this["6"] = gaf._AssetPreload.Sequences;
-    this["7"] = gaf._AssetPreload.TextFields;
-    this["8"] = gaf._AssetPreload.Atlases; // 2
-    this["9"] = gaf._AssetPreload.Stage;
-    this["10"] = gaf._AssetPreload.AnimationObjects; //2
-    this["11"] = gaf._AssetPreload.AnimationMasks; // 2
-    this["12"] = gaf._AssetPreload.AnimationFrames; // 2
-    this["13"] = gaf._AssetPreload.TimeLine;
-
-    this.Tag = function(asset, tag, timeLine)
-    {
-        (this[tag.tagId])(asset, tag.content, timeLine);
-    };
-
-    this.Tags = function(asset, tags, timeLine)
-    {
-        var self = this;
-        tags.forEach(function(tag)
-        {
-            self.Tag(asset, tag, timeLine);
-        });
-    };
+    this["0"] = this.End;
+    this["1"] = this.Atlases;
+    this["2"] = this.AnimationMasks;
+    this["3"] = this.AnimationObjects;
+    this["4"] = this.AnimationFrames;
+    this["5"] = this.NamedParts;
+    this["6"] = this.Sequences;
+    this["7"] = this.TextFields;
+    this["8"] = this.Atlases; // 2
+    this["9"] = this.Stage;
+    this["10"] = this.AnimationObjects; //2
+    this["11"] = this.AnimationMasks; // 2
+    this["12"] = this.AnimationFrames; // 2
+    this["13"] = this.TimeLine;
 };
 
-gaf._AssetPreload.End = function(asset, content, timeLine){
+gaf._AssetPreload.prototype.End = function(asset, content, timeLine){
     if(timeLine)
     {
         timeLine.getFps = function()
@@ -53,7 +39,21 @@ gaf._AssetPreload.End = function(asset, content, timeLine){
     }
 };
 
-gaf._AssetPreload.Atlases = function(asset, content, timeLine)
+gaf._AssetPreload.prototype.Tag = function(asset, tag, timeLine)
+{
+    (this[tag.tagId]).call(this, asset, tag.content, timeLine);
+};
+
+gaf._AssetPreload.prototype.Tags = function(asset, tags, timeLine)
+{
+    var self = this;
+    tags.forEach(function(tag)
+    {
+        self.Tag(asset, tag, timeLine);
+    });
+};
+
+gaf._AssetPreload.prototype.Atlases = function(asset, content, timeLine)
 {
     asset._atlasScales[content.scale] = asset._atlasScales[content.scale] || [];
     var spriteFrames = asset._atlasScales[content.scale];
@@ -165,7 +165,7 @@ gaf._AssetPreload.Atlases = function(asset, content, timeLine)
 
 };
 
-gaf._AssetPreload.AnimationObjects = function(asset, content, timeLine)
+gaf._AssetPreload.prototype.AnimationObjects = function(asset, content, timeLine)
 {
     content.forEach(function(item)
     {
@@ -175,56 +175,51 @@ gaf._AssetPreload.AnimationObjects = function(asset, content, timeLine)
     });
 };
 
-gaf._AssetPreload.AnimationFrames = function(asset, content, timeLine)
+gaf._AssetPreload.prototype.convertTint = function(mat, alpha)
 {
+    if(!mat)
+        return null;
+    return {
+        mult:
+        {
+            r: mat.redMultiplier * 255,
+            g: mat.greenMultiplier * 255,
+            b: mat.blueMultiplier * 255,
+            a: alpha * 255
+        },
+        offset:
+        {
+            r: mat.redOffset * 255,
+            g: mat.greenOffset * 255,
+            b: mat.blueOffset * 255,
+            a: mat.alphaOffset * 255
+        }
+    };
+};
+
+gaf._AssetPreload.prototype.convertState = function(state)
+{
+    return {
+        hasColorTransform: state.hasColorTransform,
+        hasMask: state.hasMask,
+        hasEffect: state.hasEffect,
+        objectIdRef: state.objectIdRef,
+        depth: state.depth,
+        alpha: state.alpha * 255,
+        matrix: gaf.CGAffineTransformCocosFormatFromFlashFormat(state.matrix),
+        colorTransform: this.convertTint(state.colorTransform, state.alpha),
+        effect: state.effect,
+        maskObjectIdRef: state.maskObjectIdRef
+    };
+};
+
+gaf._AssetPreload.prototype.AnimationFrames = function(asset, content, timeLine)
+{
+    var self = this;
     cc.assert(timeLine, "Error. Time Line should not be null.");
-
-    var convertTint = function(mat, alpha)
-    {
-        if(!mat)
-            return null;
-        var result =
-        {
-            mult:
-            {
-                r: mat.redMultiplier * 255,
-                g: mat.greenMultiplier * 255,
-                b: mat.blueMultiplier * 255,
-                a: alpha * 255
-            },
-            offset:
-            {
-                r: mat.redOffset * 255,
-                g: mat.greenOffset * 255,
-                b: mat.blueOffset * 255,
-                a: mat.alphaOffset * 255
-            }
-        };
-        return result;
-    };
-
-    var convertState = function(state)
-    {
-        var result =
-        {
-            hasColorTransform: state.hasColorTransform,
-            hasMask: state.hasMask,
-            hasEffect: state.hasEffect,
-            objectIdRef: state.objectIdRef,
-            depth: state.depth,
-            alpha: state.alpha * 255,
-            matrix: gaf.CGAffineTransformCocosFormatFromFlashFormat(state.matrix),
-            colorTransform: convertTint(state.colorTransform, state.alpha),
-            effect: state.effect,
-            maskObjectIdRef: state.maskObjectIdRef
-        };
-        return result;
-    };
-
     var statesForId = {};
     var frames = [];
     var lastFrame = {};
-
     for(var i = 0, len = content.length; i < len; ++i)
     {
         var frame = content[i];
@@ -232,15 +227,16 @@ gaf._AssetPreload.AnimationFrames = function(asset, content, timeLine)
         {
             frame.state.forEach(function (state)
             {
-                if (state.alpha !== 0) {
-                    statesForId[state.objectIdRef] = convertState(state);
+                if (state.alpha !== 0)
+                {
+                    statesForId[state.objectIdRef] = self.convertState(state);
                 }
-                else {
+                else
+                {
                     statesForId[state.objectIdRef] = null;
                 }
             });
         }
-
         var stateArray = [];
         for(var obj in statesForId){ if(statesForId.hasOwnProperty(obj) && statesForId[obj])
         {
@@ -252,7 +248,7 @@ gaf._AssetPreload.AnimationFrames = function(asset, content, timeLine)
     timeLine.getFrames = function(){return frames};
 };
 
-gaf._AssetPreload.NamedParts = function(asset, content, timeLine)
+gaf._AssetPreload.prototype.NamedParts = function(asset, content, timeLine)
 {
     var parts = {};
     content.forEach(function(item)
@@ -262,7 +258,7 @@ gaf._AssetPreload.NamedParts = function(asset, content, timeLine)
     timeLine.getNamedParts = function(){return parts};
 };
 
-gaf._AssetPreload.Sequences = function(asset, content, timeLine)
+gaf._AssetPreload.prototype.Sequences = function(asset, content, timeLine)
 {
     var sequences = {};
     content.forEach(function(item){
@@ -271,12 +267,12 @@ gaf._AssetPreload.Sequences = function(asset, content, timeLine)
     timeLine.getSequences = function(){return sequences};
 };
 
-gaf._AssetPreload.TextFields = function(asset, content, timeLine)
+gaf._AssetPreload.prototype.TextFields = function(asset, content, timeLine)
 {
     debugger;
 };
 
-gaf._AssetPreload.Stage = function(asset, content, timeLine)
+gaf._AssetPreload.prototype.Stage = function(asset, content, timeLine)
 {
     asset._sceneFps = content.fps;
     asset._sceneColor = content.color;
@@ -284,7 +280,7 @@ gaf._AssetPreload.Stage = function(asset, content, timeLine)
     asset._sceneHeight = content.height;
 };
 
-gaf._AssetPreload.AnimationMasks = function(asset, content, timeLine)
+gaf._AssetPreload.prototype.AnimationMasks = function(asset, content, timeLine)
 {
     content.forEach(function(item)
     {
@@ -294,11 +290,11 @@ gaf._AssetPreload.AnimationMasks = function(asset, content, timeLine)
     });
 };
 
-gaf._AssetPreload.TimeLine = function(asset, content, timeLine)
+gaf._AssetPreload.prototype.TimeLine = function(asset, content, timeLine)
 {
     var result = new gaf._TimeLineProto(asset, content.animationFrameCount, content.boundingBox, content.pivotPoint, content.id, content.linkageName);
     asset._pushTimeLine(result);
-    gaf._AssetPreload.Tags(asset, content.tags, result);
+    this.Tags(asset, content.tags, result);
 };
 
 gaf._AssetPreload = new gaf._AssetPreload();
